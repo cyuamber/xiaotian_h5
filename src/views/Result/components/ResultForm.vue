b9e7b2d187ce500dcf2ab96a821600da1228c529
wangyuerg <wangyuerg@chinamobile.com>
2020-11-03 17:36:56 +0800

    fix: fix the bug of resultform page

--- src/views/Result/components/ResultForm.vue
+++ src/views/Result/components/ResultForm.vue
@@ -76,7 +76,7 @@
         <img src="@/assets/images/submit-success.png" alt="提交成功">
       </div>
       <Loading v-if="LoadingShow" />
-      <p class="footer-text">
+      <p class="footer-text" v-if="beforSubmit">
         *可通过手动填写或上传名片来录入您的信息
       </p>
     </van-popup>
